202a6fa4bcb37b1e3dfadf2f0ff8c48229513563
Rafael Fagundes <fagundesrafael88@gmail.com>
2024-02-07 19:11:14 -0400

    first commit

---
+++ app/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+} from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsC4EQHB7ao0B98FHKWn76a8_VetcaJ_8",
+  authDomain: "idealapp-da571.firebaseapp.com",
+  projectId: "idealapp-da571",
+  storageBucket: "idealapp-da571.appspot.com",
+  messagingSenderId: "579274400407",
+  appId: "1:579274400407:web:865cc7d97fee4afebabfd2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+
+const db = getFirestore(app)
+
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,14 +1,54 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "../ui/login/login.module.css";
+import { auth } from "../firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter()
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user)
+        router.push("/painel")
+
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <form onSubmit={handleLogin} className={styles.form}>
         <h1>Login</h1>
-        <input type="text" id="username" placeholder="usuário" />
-        <input type="password" id="password" placeholder="senha" />
-        <button>Entrar</button>
+        <input
+          type="email"
+          id="email"
+          placeholder="insira e-mail de usuário"
+          required
+          autoComplete="on"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="informe a senha de usuário"
+          required
+          autoComplete="off"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Entrar</button>
+        {error && <span>Email e/ou senha incorreto(s)!</span>}
       </form>
     </div>
   );
